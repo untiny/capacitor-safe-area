c4c6b295fd2f21db17418dad0dc70ccdea25bb2c
qzq <837233287@qq.com>
2024-01-09 10:38:04 +0800

    优化调整

--- android/src/main/java/cn/ooix/capacitor/safearea/SafeArea.java
+++ android/src/main/java/cn/ooix/capacitor/safearea/SafeArea.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.Insets;
 import android.os.Build;
-import android.util.Log;
 import android.view.DisplayCutout;
 import android.view.WindowInsets;
 import android.widget.FrameLayout;
@@ -14,52 +13,41 @@ import com.getcapacitor.JSObject;
 public class SafeArea {
 
     private final AppCompatActivity activity;
-    private final float density;
 
     public SafeArea(AppCompatActivity activity) {
         this.activity = activity;
-        this.density = activity.getApplicationContext().getResources().getDisplayMetrics().density;
     }
 
     public JSObject getSafeArea() {
-        float left = 0, right = 0, top = 0, bottom = 0;
+        int top = 0, right = 0, bottom = 0, left = 0;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             FrameLayout content = this.activity.getWindow().getDecorView().findViewById(android.R.id.content);
             Insets windowInsets = content.getRootView().getRootWindowInsets().getInsetsIgnoringVisibility(WindowInsets.Type.systemBars());
-            left = windowInsets.left;
-            right = windowInsets.right;
             top = windowInsets.top;
+            right = windowInsets.right;
             bottom = windowInsets.bottom;
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            WindowInsets windowInsets = this.activity.getWindow().getDecorView().getRootWindowInsets();
-            final DisplayCutout displayCutout = windowInsets.getDisplayCutout();
-            if (displayCutout != null) {
-                left = displayCutout.getSafeInsetLeft();
-                right = displayCutout.getSafeInsetRight();
-                top = displayCutout.getSafeInsetTop();
-                bottom = displayCutout.getSafeInsetBottom();
-            }
-            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
-                Insets insets = windowInsets.getSystemWindowInsets();
-                left = Math.max(left, insets.left) / this.density;
-                right = Math.max(right, insets.right) / this.density;
-                top = Math.max(top, insets.top) / this.density;
-                bottom = Math.max(bottom, insets.bottom) / this.density;
-            }
+            left = windowInsets.left;
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             WindowInsets windowInsets = this.activity.getWindow().getDecorView().getRootWindowInsets();
-            left = windowInsets.getStableInsetLeft() / this.density;
-            right = windowInsets.getStableInsetRight() / this.density;
-            top = windowInsets.getStableInsetTop() / this.density;
-            bottom = windowInsets.getStableInsetBottom() / this.density;
+            top = windowInsets.getStableInsetTop();
+            right = windowInsets.getStableInsetRight();
+            bottom = windowInsets.getStableInsetBottom();
+            left = windowInsets.getStableInsetLeft();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                DisplayCutout displayCutout = windowInsets.getDisplayCutout();
+                if (displayCutout != null) {
+                    top = Math.max(displayCutout.getSafeInsetTop(), windowInsets.getStableInsetTop());
+                    right = Math.max(displayCutout.getSafeInsetRight(), windowInsets.getStableInsetRight());
+                    bottom = Math.max(displayCutout.getSafeInsetBottom(), windowInsets.getStableInsetBottom());
+                    left = Math.max(displayCutout.getSafeInsetLeft(), windowInsets.getStableInsetLeft());
+                }
+            }
         }
         JSObject ret = new JSObject();
-        ret.put("top", top);
-        ret.put("bottom", bottom);
-        ret.put("left", left);
-        ret.put("right", right);
-
-        Log.i("get safe area", ret.toString());
+        ret.put("top", this.dpToPixels(top));
+        ret.put("right", this.dpToPixels(right));
+        ret.put("bottom", this.dpToPixels(bottom));
+        ret.put("left", this.dpToPixels(left));
         return ret;
     }
 
@@ -69,8 +57,12 @@ public class SafeArea {
         Resources resources = this.activity.getResources();
         @SuppressLint({ "InternalInsetResource", "DiscouragedApi" })
         int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) ret.put("height", resources.getDimensionPixelSize(resourceId) / this.density);
-        Log.i("get status bar height", ret.toString());
+        if (resourceId > 0) ret.put("height", this.dpToPixels(resources.getDimensionPixelSize(resourceId)));
         return ret;
+    }
+
+    private int dpToPixels(int dp) {
+        float density = this.activity.getApplicationContext().getResources().getDisplayMetrics().density;
+        return (int) Math.round(dp / density);
     }
 }
